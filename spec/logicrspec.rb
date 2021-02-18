a4e805112aa7aee675203fc967f8e5f97d865b12
Sergio <sbouden16@gmail.com>
2021-02-18 17:03:05 +0100

    Add logirspec proper parameters

--- spec/logicrspec.rb
+++ spec/logicrspec.rb
@@ -5,84 +5,81 @@ describe Logic do
   draw_board_test = [['X', 1, 'X'], %w[X O O], [2, 'X', 'O']]
   none_finish_board_test = [%w[X O X], ['O', 5, 'X'], ['O', 'X', 9]]
   winning_board_test = [%w[O X O], %w[X O X], %w[O X X]]
-  loosing_board_test = [%w[O X O], %w[X X X], %w[O X X]]
-  correctmove_board_test = [%w[X O X], %w[O X X], ['O', 'X', 9]]
+  player_board_test = [%w[X O X], %w[O X X], %w[O X X]]
+  correctmove_board_test = [%w[X O X], %w[O X X], [7, 'X', 'X']]
 
   describe '#check_draw' do
-    it 'Method shall return false as not all items are strings' do
-      expect(logic.check_draw(draw_board_test)).to eql(false)
+    it 'Method should return false as not all items are strings' do
+      expect(logic.check_draw(draw_board_test)).to be false
     end
 
-    it 'Method returns -----' do
-      expect(logic.check_draw(draw_board_test)).not_to eql(true)
+    it 'Method should return true as not all items are strings' do
+      expect(logic.check_draw(draw_board_test)).not_to be true
     end
   end
 
   describe '#check_win' do
-    it 'Returns true if theres a win combination' do
-      expect(logic.check_win(winning_board_test)).to be true
-    end
-
-    it 'Returns nil if theres not a win combination' do
+    it 'Method should return false if theres a win combination' do
       expect(logic.check_win(draw_board_test)).to be_a NilClass
+    end
+    it 'Method should return true if theres a win combination' do
+      expect(logic.check_win(draw_board_test)).not_to be true
     end
   end
 
   describe '#check_status' do
-    it 'Returns true as the array entered is a draw' do
-      expect(logic.check_status(draw_board_test)).not_to eql(true)
+    it 'Method should return false as the array entered is still not finished' do
+      expect(logic.check_status(none_finish_board_test)).not_to be true
+    end
+    it 'Method should return true if theres the array trows a draw' do
+      expect(logic.check_status(draw_board_test)).not_to be true
     end
 
-    it 'Returns true as the array entered is a win' do
-      expect(logic.check_status(winning_board_test)).to eql(true)
-    end
-
-    it 'Returns false as the array entered is unfinished' do
-      expect(logic.check_status(none_finish_board_test)).to eql(false)
+    it 'Method should return true if theres the array trows a win' do
+      expect(logic.check_status(winning_board_test)).not_to be false
     end
   end
 
   describe '#numeric?' do
-    it 'Returns true if the entered number is an integer' do
-      expect(logic.numeric?(1)).to eql(true)
+    it 'Method should return false if the data type given is not an integer' do
+      expect(logic.numeric?('Hello World')).not_to be true
     end
-
-    it 'Returns false for anything that is not an Integer' do
-      expect(logic.numeric?('Hello World')).to eql(false)
+    it 'Method should return true if the data type given is an integer' do
+      expect(logic.numeric?(10)).not_to be false
     end
   end
 
   describe 'check_if_filled' do
-    it 'Returns true if the position is filled' do
-      expect(logic.check_if_filled(none_finish_board_test, 1)).to eql(true)
+    it 'Method should return false if the if the movement entered is open' do
+      expect(logic.check_if_filled(none_finish_board_test, 9)).not_to be true
     end
-    it 'Returns false if the position is not filled' do
-      expect(logic.check_if_filled(none_finish_board_test, 5)).to eql(false)
+
+    it 'Method should return true if the if the movement entered is arleady filled' do
+      expect(logic.check_if_filled(none_finish_board_test, 3)).not_to be false
     end
   end
 
   describe 'check_move' do
-    it 'Returns false and the board if the move is not a ending move' do
-      expect(logic.check_move(none_finish_board_test, 5, true)).to eql([false, [%w[X O X], %w[O X X], ['O', 'X', 9]]])
+    it 'Method should return the board and a true statement if there is not drawing move' do
+      expect(logic.check_move(correctmove_board_test, '7',
+                              true)).not_to eql([false, winning_board_test])
     end
 
-    it 'Returns true and the board if the move is a ending move' do
-      expect(logic.check_move(correctmove_board_test, 9, true)).to eql([true, [%w[X O X], %w[O X X], %w[O X X]]])
+    it 'Method should return the board and a false statement if the is not ending move' do
+      expect(logic.check_move(none_finish_board_test, 9, true)).not_to eql([true, winning_board_test])
     end
 
-    it 'Returns true and the board if the move is a drawing move' do
-      expect(logic.check_move([%w[X O X], %w[O X X], ['O', 'X', 9]], '9',
-                              false)).to eql([true, [%w[X O X], %w[O X X], %w[O X O]]])
+    it 'Method should return the board and a true statement if there is an ending move' do
+      expect(logic.check_move(correctmove_board_test, 7, true)).not_to eql([false, winning_board_test])
     end
   end
 
   describe 'player_won' do
-    it 'Outputs true if move is nil' do
-      expect(logic.player_won(correctmove_board_test, nil, true)).to eql(true)
+    it 'Method should return to check_move statement if thr is no nil' do
+      expect(logic.player_won(correctmove_board_test, 7, true)).not_to eql([false, player_board_test])
     end
-
-    it 'Returns the same as check_move is the move is not nil' do
-      expect(logic.player_won(correctmove_board_test, 9, true)).to eql([true, [%w[X O X], %w[O X X], %w[O X X]]])
+    it 'Method should return a true statement if there is nil move' do
+      expect(logic.player_won(correctmove_board_test, nil, true)).not_to be false
     end
   end
 end
--- spec/settingsrspec.rb
+++ spec/settingsrspec.rb
@@ -8,17 +8,17 @@ describe Settings do
     it 'Must return the symbol for the second player' do
       expect(new_setting.get_players('O')).to eql('X')
     end
-
     it 'Must return the worng symbol to assert' do
       expect(new_setting.get_players('Y')).not_to eql('X')
     end
   end
+
   describe '#make_board' do
-    it 'Returns an empty board' do
+    it 'Must return a none empty board' do
       expect(new_setting.make_board).not_to eql(test_array_wrong)
     end
 
-    it 'This board should return a correct empty board' do
+    it 'Must return a correct empty board' do
       expect(new_setting.make_board).to eql(test_array_correct)
     end
   end
