52f1ee32f90092f12b93a3f30ca8fbe994d2eeb6
Billo Diallo <billodiallone@gmail.com>
2021-02-19 07:38:30 +0300

    testing get_players method

--- spec/settingsrspec.rb
+++ spec/settingsrspec.rb
@@ -5,19 +5,19 @@ describe Settings do
 
   test_array_wrong = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
   test_array_correct = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  describe '#get_players' do
-    it 'Must return the symbol for the second player' do
-      expect(new_setting.get_players('O')).to eql('X')
-    end
-    it 'Must return the worng symbol to assert' do
-      expect(new_setting.get_players('Y')).not_to eql('X')
-    end
-  end
-
-  # describe '#make_board' do
-  #   it 'Must return a none empty board' do
-  #     expect(new_setting.make_board).not_to eql(test_array_wrong)
+  # describe '#get_players' do
+  #   it 'Must return the symbol for the second player' do
+  #     expect(new_setting.get_players('O')).to eql('X')
   #   end
+  #   it 'Must return the worng symbol to assert' do
+  #     expect(new_setting.get_players('Y')).not_to eql('X')
+  #   end
+  # end
+
+  describe '#make_board' do
+    it 'Must return a none empty board' do
+      expect(new_setting.make_board).not_to eql(test_array_wrong)
+    end
 
     it 'Must return a correct empty board' do
       expect(new_setting.make_board).to eql(test_array_correct)
