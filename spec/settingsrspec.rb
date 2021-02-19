010247cf1fac22d173c3c462a980d588bdbe13cb
Billo Diallo <billodiallone@gmail.com>
2021-02-19 07:40:06 +0300

    update testing get_players method and fix one error

--- spec/settingsrspec.rb
+++ spec/settingsrspec.rb
@@ -5,14 +5,14 @@ describe Settings do
 
   test_array_wrong = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
   test_array_correct = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  # describe '#get_players' do
-  #   it 'Must return the symbol for the second player' do
-  #     expect(new_setting.get_players('O')).to eql('X')
-  #   end
-  #   it 'Must return the worng symbol to assert' do
-  #     expect(new_setting.get_players('Y')).not_to eql('X')
-  #   end
-  # end
+  describe '#get_players' do
+    it 'Must return the symbol for the second player' do
+      expect(new_setting.get_players('O')).to eql('X')
+    end
+    it 'Must return the worng symbol to assert' do
+      expect(new_setting.get_players('Y')).not_to eql('X')
+    end
+  end
 
   describe '#make_board' do
     it 'Must return a none empty board' do
